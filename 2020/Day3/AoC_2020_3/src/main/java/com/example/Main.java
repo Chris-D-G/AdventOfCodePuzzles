14a8ea40bb5d4d08ed86d859a0613242fe97a081
Chris-D-G <Chris.D.Guerrero@protonmail.com>
2023-11-27 13:05:41 -0500

    generalized solution to input slope values

--- 2020/Day3/AoC_2020_3/src/main/java/com/example/Main.java
+++ 2020/Day3/AoC_2020_3/src/main/java/com/example/Main.java
@@ -8,47 +8,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //
         File input = new File("input.txt");
-        int lineCount = 0;
+        List<String> list = createExpandedAsciiMap(input);
+        System.out.println("Tree count part 1: " + countTrees(list,3,1));
+        long slope1Results=countTrees(list,1,1);
+        long slope2Results=countTrees(list,3,1);
+        long slope3Results=countTrees(list,5,1);
+        long slope4Results=countTrees(list,7,1);
+        long slope5Results=countTrees(list,1,2);
+        System.out.println("Tree count part 2:" +(slope1Results
+                                                 *slope2Results
+                                                 *slope3Results
+                                                 *slope4Results
+                                                 *slope5Results) );
+    }
+    public static List<String> createExpandedAsciiMap(File inputFile){
         List<String> list = new ArrayList<>();
-        try( Scanner filereader = new Scanner(input) ){
-            while(filereader.hasNext()){
-                list.add(filereader.nextLine().trim()) ;
+        try( Scanner fileReader = new Scanner(inputFile) ){
+            while(fileReader.hasNext()){
+                String line =fileReader.nextLine().trim();
+                list.add(line) ;
             }
         }catch (FileNotFoundException e){
             System.out.println("Something went wrong with the file!");
+            return list;
         }
-        System.out.println("Tree count: " + countTrees(list));
-
-
+        return list;
     }
+
     public static boolean checkForTree(String input, int index){
-        // tree = '#';
-        // opening = '.';
         return input.charAt(index)=='#';
-
     }
-    public static int countTrees(List<String> asciiTreeMap){
-        // length of line :31 chars. index 0 -> 30
-        // Number of lines: 323
-        int treeCount = 0;
-        int position = 3;
-        for(int i =1; i<asciiTreeMap.size();i++){
-            if(position<=30){
-                if(checkForTree(asciiTreeMap.get(i),position)){
-                    treeCount++;
-                }
-                position+=3;
-            }else{
-                position-=31;
-                if(checkForTree(asciiTreeMap.get(i),position)){
-                    treeCount++;
-                }
+    public static long countTrees(List<String> asciiTreeMap,int xTraversal, int yTraversal){
+        long treeCount = 0;
+        int position = xTraversal;
+        for(int i =1; i<asciiTreeMap.size();i+=yTraversal){
+            if(checkForTree(asciiTreeMap.get(i),position)){
+                treeCount++;
             }
+            position+=xTraversal;
+            position%=asciiTreeMap.get(i).length();
         }
+        System.out.println("Slope: Right: "+xTraversal+" Down: "+yTraversal+" Tree Count: "+treeCount);
         return treeCount;
     }
-
-
 }
