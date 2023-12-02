05f73f835c075c9e7fa34396e5e7d5244d706501
Chris-D-G <Chris.D.Guerrero@protonmail.com>
2023-12-02 16:13:15 -0500

    Solved 2015 day 2 and refactored using streams

--- 2015/Day2/AoC_2015_2/src/main/java/org/example/Main.java
+++ 2015/Day2/AoC_2015_2/src/main/java/org/example/Main.java
@@ -2,14 +2,14 @@ package org.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Vector;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        calculateResults(parseData());
+
     }
 
     /**
@@ -30,9 +30,26 @@ public class Main {
         return list;
     }
 
-    private static long caculateResults(List<String> sizeOfPresents){
+    private static void calculateResults(List<String> sizeOfPresents){
         long wrappingPaper = 0L;
-        return 0L;
+        long requiredRibbon =0L;
+        for (String element : sizeOfPresents){
+            List<Long> dimensions= Stream.of(element.split("x"))
+                                        .map(Long::valueOf)
+                                        .sorted()
+                                        .collect(Collectors.toList());
+            System.out.println(dimensions);
+            long area1 = dimensions.get(0) * dimensions.get(1);
+            long area2 = dimensions.get(1) * dimensions.get(2);
+            long area3 = dimensions.get(0) * dimensions.get(2);
+            long currentPaper = 2*(area1+area2+area3) + area1;
+            long volume = dimensions.get(0) * dimensions.get(1) * dimensions.get(2);
+            long currentRibbon =volume + 2*(dimensions.get(0) + dimensions.get(1));
+            wrappingPaper+=currentPaper;
+            requiredRibbon+=currentRibbon;
+        }
+        System.out.println("Total paper needed: "+ wrappingPaper+ " sqft." );
+        System.out.println("Total ribbon needed: " + requiredRibbon + " ft.");
     }
 
 
